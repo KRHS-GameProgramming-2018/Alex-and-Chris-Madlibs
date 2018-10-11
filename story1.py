2be8a1487ee074af0758d45c5ca0f6208fbc46cf
Alex <alex.didominic@kearsarge.org>
2018-10-11 07:52:05 -0400

    dd

--- story1.py
+++ story1.py
@@ -6,13 +6,20 @@ def playMadlibs():
     animals1 = getWord("Enter a pluaral animal name: ")
     adjective1 = getWord("Enter a thing:")
     verb1 = getWord("Enter a action:")
+    verb2 = getWord("Enter a action:")
+    adjective2 = getWord("Enter a thing:")
+    
+    
+    
+    
+    
     
     output = ""
     output += "One day I was walking with my friend, " + friend1
     output += "And I saw a huge " + adjective1
     output += "I started to run away but it " + verb1
-    output += "after it was done it started to" + verb1
-    output += "the" + adjective1
+    output += "after it was done it started to" + verb2
+    output += "I finnally managed to get away and 
     
     
     
