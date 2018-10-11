bf8840e1685e6fa83a62af828b2e38a7b7ca34e0
christopher <christopher.richardson@kearsarge.org>
2018-10-11 14:16:52 -0400

    setup story 2 and 3

---
+++ story2.py
@@ -0,0 +1,13 @@
+from getInput import *
+
+def playMadlibs():
+    friend1 = getWord("Enter a Name: ")
+    numAnimals = getNumber("Enter a number: ", 2, 10)
+    animals1 = getWord("Enter a pluaral animal name: ")
+    adjective1 = getWord("Enter a thing:")
+    verb1 = getWord("Enter a action:")
+    
+    
+    output = ""
+    output += "One morning I saw my friend, " + friend1
+    output += "
